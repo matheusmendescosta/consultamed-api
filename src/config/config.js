20b9bfb0f5653df94bb5dc8d69963648317c79bf
matheus mendes <matheus_mendess@live.com>
2023-03-05 01:30:11 -0300

    conexão com o banco de dados

---
+++ src/config/config.js
@@ -0,0 +1,40 @@
+// const fs = require('fs');
+
+// module.exports = {
+//   development: {
+//     username: 'database_dev',
+//     password: 'database_dev',
+//     database: 'database_dev',
+//     host: '127.0.0.1',
+//     port: 3306,
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       bigNumberStrings: true
+//     }
+//   },
+//   test: {
+//     username: process.env.CI_DB_USERNAME,
+//     password: process.env.CI_DB_PASSWORD,
+//     database: process.env.CI_DB_NAME,
+//     host: '127.0.0.1',
+//     port: 3306,
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       bigNumberStrings: true
+//     }
+//   },
+//   production: {
+//     username: process.env.PROD_DB_USERNAME,
+//     password: process.env.PROD_DB_PASSWORD,
+//     database: process.env.PROD_DB_NAME,
+//     host: process.env.PROD_DB_HOSTNAME,
+//     port: process.env.PROD_DB_PORT,
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       bigNumberStrings: true,
+//       ssl: {
+//         ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+//       }
+//     }
+//   }
+// };
---
+++ src/repositories/db.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv'
+import Sequelize from "sequelize";
+
+dotenv.config()
+
+const username = process.env.username;
+const password = process.env.password;
+const database = process.env.database;
+const host = process.env.host;
+
+const sequelize = new Sequelize(
+    database, 
+    username, 
+    password, 
+    {
+    host: host,
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    }
+});
+
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão estabelecida com sucesso');
+  } catch (error) {
+    console.error('Não foi possivel estabelecer conexão com o banco de dados:', error);
+  }
+
+export default sequelize;
